87031791d551ecf636298b229c9173ad2934c383
cqc <1016903103@qq.com>
2016-11-21 20:31:41 +0800

    update port

---
+++ server/config.py
@@ -0,0 +1,5 @@
+KEY='something'
+NEED_AUTH=True
+AUTH_USER='admin'
+AUTH_PASSWORD='123456'
+PORT=8888
---
+++ server/main.py
@@ -0,0 +1,58 @@
+from flask import request, Flask, Response
+import config
+from functools import wraps
+
+
+def check_auth(username, password):
+    if config.NEED_AUTH:
+        return username == config.AUTH_USER and password == config.AUTH_PASSWORD
+    else:
+        return True
+
+
+def authenticate():
+    return Response(
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+
+    return decorated
+
+
+app = Flask(__name__)
+
+
+@app.route('/record/<key>', methods=['GET'])
+def record(key):
+    if key == config.KEY:
+        ip = request.remote_addr
+        with open('ip', 'w') as f:
+            f.write(ip)
+            f.close()
+        return ip + '\n'
+    else:
+        return 'Invalid Key'
+
+
+@app.route('/proxy', methods=['GET'])
+@requires_auth
+def proxy():
+    with open('ip', 'r') as f:
+        ip = f.read().strip()
+        f.close()
+        if ip:
+            return ip + ':' + str(config.PORT)
+    return '0'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
